5306059753d1c56fb292d7a692e595ef26950650
miscellian <120700max@gmail.com>
2021-12-28 01:29:49 +0200

    modify voice classifier method docs

--- src/lorem_generator.py
+++ src/lorem_generator.py
@@ -1,4 +1,4 @@
 import lorem, random
 
 def generate(len=None):
-    return " ".join([lorem.sentence() for i in range(len if len != None else random.randint(10, 30))])
+    return " ".join([lorem.sentence() for i in range(len if len != None else random.randint(5, 15))])
--- src/voice_classifier.py
+++ src/voice_classifier.py
@@ -43,6 +43,14 @@ class VoiceClassifier:
         self.debug = debug
         
     def train_model(self, mp3, csv, save_model_path="model"):
+        """
+        Public method
+        Preparing data & trains CNN model
+        Params:
+        mp3 - path to mp3 data
+        csv - path to csv data
+        save_model_path - url for saving model
+        """
         self.number_of_components = 4
         self.build_spectrogram(mp3)
         self.read_csv(csv)
@@ -54,9 +62,21 @@ class VoiceClassifier:
         self.save_model(save_model_path)
         
     def exploit_model(self, load_model_path):
+        """
+        Public method
+        Use model
+        Params:
+        load_model_path - url for loading model
+        """
         self.model_predict(load_model_path)
         
     def build_spectrogram(self, mp3):
+        """
+        Private method
+        Build Mel-spectrogram, convert amplitude square to db
+        Params:
+        mp3 - path to mp3 data
+        """
         y, sr = librosa.load(mp3)
         self.duration = librosa.get_duration(y, sr=sr)
         S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=20, fmax=8000)
@@ -69,6 +89,12 @@ class VoiceClassifier:
             plt.show()
         
     def read_csv(self, csv):
+        """
+        Private method
+        Reading csv data and do some transformations
+        Params:
+        csv - path to csv data
+        """
         data = pd.read_csv(csv)
         if self.debug:
             print("\n")
@@ -82,12 +108,16 @@ class VoiceClassifier:
             plt.show()
             
     def process_pca_data(self):
+        """
+        Private method
+        Use PCA to reduce number of features
+        """
         audio_data = pd.DataFrame(self.S_dB)
         audio_data = audio_data.T
         if self.debug:
             print("\n")
             print("Audio data DF")
-            display(audio_data.tail())
+            display(audio_data.head(10))
         pca = PCA(n_components=self.number_of_components)
         pca.fit(audio_data)
         self.pca_data = pd.DataFrame(pca.transform(audio_data))
@@ -113,6 +143,10 @@ class VoiceClassifier:
             plt.show()
         
     def select_most_informative_component(self):
+        """
+        Private method
+        Select the most informative component via mutual information from sklearn
+        """
         components = list(range(self.number_of_components))
         components.insert(0, "Time")
         mi_array = mi(X=self.pca_data[components], y=self.pca_data["speaker"])
@@ -125,6 +159,10 @@ class VoiceClassifier:
             print("The most informative is %d component" % (self.mic))
         
     def process_time_series_data(self):
+        """
+        Private method
+        Using labelencoder for speaker class
+        """
         self.time_series_data = self.pca_data[["Time", self.mic, "speaker"]]
         self.time_series_data = self.time_series_data.rename(columns={self.mic: "X"})
         if self.debug:
@@ -134,8 +172,15 @@ class VoiceClassifier:
             plt.show()
         label_encoder = LabelEncoder()
         self.time_series_data['speaker'] = label_encoder.fit_transform(self.time_series_data.speaker)
+        if self.debug:
+            sns.scatterplot(x='Time',y='X',hue='speaker',data=self.time_series_data,s=10,palette='plasma')
+            plt.show()
 
     def prepare_model(self):
+        """
+        Private method
+        Prepare data for CNN model, split data to test and train, mix train data
+        """
         self.speaker_list = self.new_data.speaker.unique().tolist()
         self.X = self.time_series_data[['Time','X']]
         self.y = self.time_series_data.speaker
@@ -145,46 +190,62 @@ class VoiceClassifier:
         x_train = np.array(X_train).reshape((np.array(X_train).shape[0], np.array(X_train).shape[1], 1))
         self.y_test = np.array(y_test)
         self.x_test = np.array(X_test).reshape((np.array(X_test).shape[0], np.array(X_test).shape[1], 1))
-        num_classes = len(np.unique(y_train))
+        number_of_classes = len(np.unique(y_train))
         idx = np.random.permutation(len(x_train))
         self.x_train = x_train[idx]
         self.y_train = y_train[idx]
         self.X_test = X_test
-        self.model = self.make_model(self.x_train.shape[1:], num_classes)
+        self.model = self.make_model(self.x_train.shape[1:], number_of_classes)
 
-    def make_model(self, input_shape, num_classes):
-        input_layer = keras.layers.Input(input_shape)
-
-        conv1 = keras.layers.Conv1D(filters=64, kernel_size=3, padding="same")(input_layer)
-        conv1 = keras.layers.BatchNormalization()(conv1)
-        conv1 = keras.layers.ReLU()(conv1)
-
-        conv2 = keras.layers.Conv1D(filters=64, kernel_size=3, padding="same")(conv1)
-        conv2 = keras.layers.BatchNormalization()(conv2)
-        conv2 = keras.layers.ReLU()(conv2)
-
-        conv3 = keras.layers.Conv1D(filters=64, kernel_size=3, padding="same")(conv2)
-        conv3 = keras.layers.BatchNormalization()(conv3)
-        conv3 = keras.layers.ReLU()(conv3)
-
-        gap = keras.layers.GlobalAveragePooling1D()(conv3)
-
-        output_layer = keras.layers.Dense(num_classes, activation="softmax")(gap)
-
-        return keras.models.Model(inputs=input_layer, outputs=output_layer)
+    def make_model(self, shape, number_of_classes):
+        """
+        Private method
+        Create 3-layer CNN model
+        Params:
+        shape - shape of input data
+        number_of_classes - number of data classes
+        """
+        inputl = keras.layers.Input(shape)
+        layer_1 = keras.layers.Conv1D(50, 3, padding="same")(inputl)
+        layer_1 = keras.layers.BatchNormalization()(layer_1)
+        layer_1 = keras.layers.ReLU()(layer_1)
+        layer_2 = keras.layers.Conv1D(50, 3, padding="same")(layer_1)
+        layer_2 = keras.layers.BatchNormalization()(layer_2)
+        layer_2 = keras.layers.ReLU()(layer_2)
+        layer_3 = keras.layers.Conv1D(50, 3, padding="same")(layer_2)
+        layer_3 = keras.layers.BatchNormalization()(layer_3)
+        layer_3 = keras.layers.ReLU()(layer_3)
+        global_average_pooling = keras.layers.GlobalAveragePooling1D()(layer_3)
+        outputl = keras.layers.Dense(number_of_classes, activation="softmax")(global_average_pooling)
+        return keras.models.Model(inputs=inputl, outputs=outputl)
 
     def keras_train_model(self, epochs, batch_size):
+        """
+        Private method
+        Train CNN model via keras with some callbacks and adam optimizer
+        Params:
+        epochs - number of epochs
+        batch_size - size of the batch
+        """
         epochs = epochs
         batch_size = batch_size
-
         callbacks = [
             keras.callbacks.ModelCheckpoint(
-                "best_model.h5", save_best_only=True, monitor="val_loss"
+                "best_model.h5", 
+                save_best_only=True, 
+                monitor="val_loss"
             ),
             keras.callbacks.ReduceLROnPlateau(
-                monitor="val_loss", factor=0.5, patience=20, min_lr=0.0001
+                monitor="val_loss", 
+                factor=0.5, 
+                patience=20, 
+                min_lr=0.0001
             ),
-            keras.callbacks.EarlyStopping(monitor="val_loss", patience=50, verbose=1),
+            keras.callbacks.EarlyStopping(
+                monitor="val_loss",
+                patience=50, 
+                verbose=1
+            ),
         ]
         print("\n")
         print("Training model. Epochs: %d. Batch size: %d" % (epochs, batch_size))
@@ -203,24 +264,36 @@ class VoiceClassifier:
             verbose=1,
         )
         if self.debug:
-            test_loss, test_acc = self.model.evaluate(self.x_test, self.y_test)
+            loss, acc = self.model.evaluate(self.x_test, self.y_test)
             print("\n")
-            print("Test accuracy: %.4f" % test_acc)
-            print("Test loss: %.4f" % test_loss)
+            print("Test accuracy: %.4f" % acc)
+            print("Test loss: %.4f" % loss)
             
     def save_model(self, save_model_path):
+        """
+        Private method
+        Save model to disk storage
+        Params:
+        save_model_path - url for saving model
+        """
         self.model.save(save_model_path)
         if self.debug:
             print("Model was saved to ./%s" % save_model_path)
             
         
     def model_predict(self, load_model_path):
+        """
+        Private method
+        Make prediction on test data via CNN trained model
+        Params:
+        load_model_path - url for loading model
+        """
         self.model = keras.models.load_model(load_model_path)
         pred_test = pd.DataFrame(self.model.predict(self.x_test))
         for i in range(len(self.speaker_list)):   
             pred_test = pred_test.rename(columns={i: self.speaker_list[i]})
         if self.debug:
-            display(pred_test.tail())
+            display(pred_test.head(10))
         test_data = self.X_test.reset_index().drop('index', axis=1)
         test_data['target'] = self.y_test
         for i in range(len(self.speaker_list)):   
@@ -233,31 +306,42 @@ class VoiceClassifier:
         for i in range(len(self.speaker_list)):   
             plt.title('%s Probability' % self.speaker_list[i])
             sns.scatterplot(test_data.Time, test_data.X, s=10, hue=test_data[self.speaker_list[i]])
-            plt.grid(True)
             plt.show()
         if self.debug:
             self.create_classiffication_report()
             
             
     def create_classiffication_report(self):
-        cm = confusion_matrix(self.model_to_clf(self.x_test), self.y_test)
-        cm_data = pd.DataFrame(cm, columns=[self.speaker_list[0], self.speaker_list[1]])
-        cm_data.index = [self.speaker_list[0], self.speaker_list[1]]
-        sns.heatmap(cm_data.astype(int), fmt='d', annot=True,cmap='plasma')
+        """
+        Private method
+        Generate confusion matrix and classification report from sklearn
+        Params:
+        load_model_path - url for loading model
+        """
+        conf_matrix = confusion_matrix(self.prepare_data_for_report(self.x_test), self.y_test)
+        conf_matrix_data = pd.DataFrame(conf_matrix, columns=self.speaker_list)
+        conf_matrix_data.index = self.speaker_list
+        sns.heatmap(conf_matrix_data.astype(int), fmt='d', annot=True, cmap='plasma')
         plt.yticks(rotation=0)
-        plt.xticks(rotation=0,fontsize=10)
+        plt.xticks(rotation=0, fontsize=10)
         plt.show()
-        clf_report = classification_report(self.y_test,
-                                   self.model_to_clf(self.x_test),
+        report = classification_report(self.y_test,
+                                   self.prepare_data_for_report(self.x_test),
                                    labels=[0, 1, 2],
-                                   target_names=[self.speaker_list[0], self.speaker_list[1]],
+                                   target_names=self.speaker_list,
                                    output_dict=True)
-        sns.heatmap(pd.DataFrame(clf_report).iloc[:-1, :].T, annot=True,cmap='plasma')
+        sns.heatmap(pd.DataFrame(report).iloc[:-1, :].T, annot=True, cmap='plasma')
         plt.show()
     
-    def model_to_clf(self, input_data):
-        pred_list = self.model.predict(input_data)
-        LIST_PRED = []
-        for p in pred_list:
-            LIST_PRED.append(np.argmax(p))
-        return LIST_PRED
+    def prepare_data_for_report(self, data):
+        """
+        Private method
+        Prepare input data for classification report
+        Params:
+        data - data for preparing
+        """
+        predictions = self.model.predict(data)
+        data_for_report = []
+        for p in predictions:
+            data_for_report.append(np.argmax(p))
+        return data_for_report
